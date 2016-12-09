3fa41136d34ac9e51a782e7b9819a7382aedd92f
2hill <jamil.rahou@laposte.net>
2016-12-09 09:44:57 +0100

    modif code background

--- js/uppertails.js
+++ js/uppertails.js
@@ -1,3 +1,18 @@
+// Début code Jamil pour le mouvement de la souris
+cursor("NONE"); // remove mouse cursor
+
+// décla des variables:
+
+var heart = new Image();
+var background = new Image();
+var x = 0;
+var y = 0;
+
+//linker les images aux var
+heart.src = "img/heart.png";
+background.src = "img/background.png";
+
+
 window.onload = function() {
 	//recupérer zone dessin canvas dans le html
 	var canvas = document.getElementById("canvas");
@@ -7,6 +22,17 @@ window.onload = function() {
 
   var W = 512;
 	var H = 256;
+
+
+
+
+  let draw = function() {
+    x = lerp(mouseX, x, 0);  // means Linear Interpolation between two points
+    y = lerp(mouseY, y, 0);
+    background(0, 0, 0); // black color background
+    image(heart, x - 50, y - 50, 100, 100);
+};
+
 
     init();
 
