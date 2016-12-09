f0ffc2237c176e8f479c099483addf0031374ecc
NeoFlake <jonathan.schmidt.13@gmail.com>
2016-12-09 11:16:07 +0100

    AJOUT DES VARIABLES SOURIS, je suis con, je sais

--- js/uppertails.js
+++ js/uppertails.js
@@ -17,6 +17,7 @@ window.onload = function() {
 	b1, b2, b3, b4, b5, b6, b7, b8, b9, b10,
 	b11, b12, b13, b14, b15, b16, b17,	b18, b19, b20,
 	lB = {};
+	var mouseX, mouseY;
 
   var W = 512;
 	var H = 256;
@@ -38,5 +39,5 @@ window.onload = function() {
 			context.drawImage();
 			context.drawImage();
 		}
-		
+
 }
