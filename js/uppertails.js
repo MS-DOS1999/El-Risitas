85126cbc0e503e6a809e8e3389634eeefed2be8c
NeoFlake <jonathan.schmidt.13@gmail.com>
2016-12-09 10:13:13 +0100

    correctif stupidité.com

--- js/uppertails.js
+++ js/uppertails.js
@@ -13,12 +13,12 @@ window.onload = function() {
 	var context = canvas.getContext("2d");
 
 	// Déclaration des variables objets
-	var joueur = {};
-	var bloc1, var bloc2, var bloc3, var bloc4, var bloc5,
-	var bloc6, var bloc7, var bloc8, var bloc9, var bloc10,
-	var bloc11, var bloc12, var bloc13, var bloc14, var bloc15,
-	var bloc16, var bloc17,	var bloc18, var bloc19, var bloc20,
-	var lifeBar = {};
+	var p1 = {};
+	var b1, var b2, var b3, var b4, var b5,
+	var b6, var b7, var b8, var b9, var b10,
+	var b11, var b12, var b13, var b14, var b15,
+	var b16, var b17,	var b18, var b19, var b20,
+	var lB = {};
 
   var W = 512;
 	var H = 256;
